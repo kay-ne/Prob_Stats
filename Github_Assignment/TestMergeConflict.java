1de83ce2f47a131d76b4e0afea6f1af655d862ec
Kaysey N <85714834+kay-ne@users.noreply.github.com>
2023-09-21 22:10:27 -0400

    Created folder for github assignment and merge conflict

---
+++ Github_Assignment/TestMergeConflict.java
@@ -0,0 +1,7 @@
+public class TestMergeConflict 
+{
+    public static void main(String[]args)
+    {
+        System.out.println("This file was made to create and resolve a merge conflict.");
+    }
+}
