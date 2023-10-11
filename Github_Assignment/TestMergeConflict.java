65763b88bee854f70625332ab0da7d74ee888bae
Kaysey N. <85714834+kay-ne@users.noreply.github.com>
2023-10-11 10:29:43 -0400

    testing merge conflict

--- Github_Assignment/TestMergeConflict.java
+++ Github_Assignment/TestMergeConflict.java
@@ -6,6 +6,5 @@ public class TestMergeConflict
     {
         System.out.println("This file was made to create and resolve a merge conflict. Create a merge conflict.");
         System.out.println("Hello Heather was here!");
-        System.out.println("Attempting merge conflict!");
     }
 }
