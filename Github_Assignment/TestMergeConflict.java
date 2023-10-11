7a318396b689057a7a68f224e0544cf8a534ed5f
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-10-11 10:27:37 -0400

    Linda was here

--- Github_Assignment/TestMergeConflict.java
+++ Github_Assignment/TestMergeConflict.java
@@ -7,5 +7,6 @@ public class TestMergeConflict
         System.out.println("This file was made to create and resolve a merge conflict. Create a merge conflict.");
         System.out.println("Hello Heather was here!");
         System.out.println("Attempting merge conflict!");
+        System.out.println("Linda was here!!!!!!!!");
     }
 }
